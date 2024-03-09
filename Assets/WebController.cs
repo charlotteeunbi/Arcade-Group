a23bfb5809d652e111ddee4027bc87a220c7c196
charlotteeunbi <charlottejen992@gmail.com>
2024-03-09 14:31:09 -0800

    Added web health bar (decreases with time) to web prefab

---
+++ Assets/WebController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebController : MonoBehaviour
+{
+    public Slider webHealthSlider;
+
+
+    public float webLife = 20f;
+    private float time = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        webHealthSlider.maxValue = webLife;
+        webHealthSlider.value = webLife;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= webLife) {
+            Destroy(gameObject);
+        }
+        else {
+            webHealthSlider.value = webLife - time;
+        }
+    }
+}
