311ad06c457e75b7cffe8ce273a30d8a7fef9eab
Adrian Y <66759189+adryue@users.noreply.github.com>
2024-03-14 17:25:53 -0700

    Add Enemy Spawner

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Vector3 Target;
+    public float speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveToTarget();
+    }
+
+    public void SetTarget(Vector3 target)
+    {
+        Target = target;
+    }
+
+    void moveToTarget()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, Target, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    //add later - when there are multiple enemies
+    //public GameObject[] enemies;
+    //GameObject randomEnemy = Enemy[Random.Range(0, enemies.Length)];
+    public GameObject Enemy;
+    public Transform Target;
+    public float spawnTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            spawnEnemy();
+            yield return new WaitForSeconds(spawnTime);
+        }
+    }
+
+    void spawnEnemy()
+    {
+        Vector3 spawnPosition = transform.position;
+        Instantiate(Enemy, spawnPosition, Quaternion.identity);
+        Enemy.GetComponent<EnemyMovement>().SetTarget(Target.position);
+    }
+}
