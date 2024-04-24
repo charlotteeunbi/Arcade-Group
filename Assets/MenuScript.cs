6416635a004b187bdc14661513e1b8f0fc92a5f6
charlotteeunbi <charlottejen992@gmail.com>
2024-04-23 23:39:19 -0700

    TITLE/DEATH SCENES

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    public AudioSource buttonClick;
+    public void playButton() {
+        buttonClick.Play();
+    }
+
+    public void restartGame() {
+        SceneManager.LoadScene("TitleScene");
+        
+    }
+    public void PlayGame() {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
---
+++ Assets/Replay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Replay : MonoBehaviour
+{
+    public AudioSource buttonClick;
+    public void playButton() {
+        buttonClick.Play();
+    }
+
+    public void restartGame() {
+        SceneManager.LoadScene("SampleScene");
+        
+    }
+}
