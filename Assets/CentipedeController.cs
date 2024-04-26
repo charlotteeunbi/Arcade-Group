4cb1db6ee2743063eae8a1218469b1cef2fec00e
Adrian Y <66759189+adryue@users.noreply.github.com>
2024-04-25 17:39:55 -0700

    added centipede death animation functionality when touching webs & target

--- Assets/CentipedeController.cs
+++ Assets/CentipedeController.cs
@@ -5,10 +5,10 @@ using static UnityEngine.GraphicsBuffer;
 
 public class CentipedeController : MonoBehaviour
 {
-
     public float speed = 3f;
     public Rigidbody2D rb;
     public Animator anim;
+    private bool dying = false;
    // public Transform target;
 
     // Start is called before the first frame update
@@ -20,28 +20,41 @@ public class CentipedeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 target = Vector3.zero;
-        Vector2 direction = (target - transform.position).normalized;
+        if (!dying)
+        {
+            Vector3 target = Vector3.zero;
+            Vector2 direction = (target - transform.position).normalized;
 
-        rb.velocity = direction * speed;
+            rb.velocity = direction * speed;
 
-        if (direction != Vector2.zero) {
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            if (direction != Vector2.zero)
+            {
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
+            }
+
+            //anim.SetFloat("Speed", rb.velocity.magnitude);
+            anim.Play("CentipedeLeft");
         }
-
-   
-        anim.SetFloat("Speed", rb.velocity.magnitude);
-
-
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Web"))
+        if (other.CompareTag("Web") || other.CompareTag("Target"))
         {
-            Destroy(gameObject);
+            StartCoroutine(playDeathAnimation());
+            dying = true;
+            //anim.Play("CentipedeDeath");
+            //Destroy(gameObject);
         }
+    }
+
+    IEnumerator playDeathAnimation()
+    {
+        anim.Play("CentipedeDeath");
+        //Destroy(gameObject);
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
     }
 }
