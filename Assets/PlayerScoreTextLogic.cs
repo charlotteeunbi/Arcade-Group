1f972512ac874b18bd93fe26637fe0688c936fc7
Adrian Y <66759189+adryue@users.noreply.github.com>
2024-05-02 17:10:58 -0700

    Added UI score text for web fluid

---
+++ Assets/PlayerScoreTextLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerScoreTextLogic : MonoBehaviour
+{
+    public FluidController fluidController;
+    public TextMeshProUGUI textComponent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        textComponent.text = ((int)fluidController.fluidSlider.value).ToString();
+    }
+}
